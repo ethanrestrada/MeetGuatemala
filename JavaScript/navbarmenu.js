c44d3aa94630242bd818c71ea95cb700f58f17b4
Ethan Ruiz <etanr25@gmail.com>
2021-05-02 22:20:20 -0600

    v.01 - header

---
+++ JavaScript/navbarmenu.js
@@ -0,0 +1,27 @@
+/*Desplegar menu*/
+let menuToggle = document.querySelector('.header__menu-toggle');
+let menuToggleIcon = document.querySelector('.header__menu-toggle i');
+let menu = document.getElementById('header__navbar');
+
+    menuToggle.addEventListener('click', e=>{
+        menu.classList.toggle('show');
+
+    if(menu.classList.contains('show')){
+        menuToggleIcon.setAttribute('class', 'fa fa-times');  
+    } else{
+        menuToggleIcon.setAttribute('class','fa fa-bars');
+    }
+});
+
+/*Fijar navbar*/
+$(document).ready(function(){
+    var altura = $('#header__navbar').offset().top;
+    
+    $(window).on('scroll', function(){
+        if ( $(window).scrollTop() > altura ){
+            $('#header__navbar').addClass('navbar-fixed');
+        } else{
+            $('#header__navbar').removeClass('navbar-fixed');
+        }
+    });
+});
